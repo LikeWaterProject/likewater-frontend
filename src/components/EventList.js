d3b50d1ddbf3171c7115dd9109b0b164a64260f4
Tobias Fried <friedtm@gmail.com>
2020-06-10 19:52:08 -0400

    EventList: prepare for generating list items from remote data

--- src/components/EventList.js
+++ src/components/EventList.js
@@ -1,63 +1,74 @@
-import React from "react";
+import React, { useMemo } from "react";
 import { Segment, Icon, Image, List } from "semantic-ui-react";
 
-const EventList = ({ events }) => (
-  <div style={{ backgroundColor: "#1E1F20", padding: 8 }}>
-    {/* <Segment inverted> */}
+const sampleEvents = [
+  {
+    type: "Water Distribution",
+    description: "",
+    distance: 250,
+    lastActive: "Just now",
+    icon: "medkit",
+    color: "red",
+  },
+  {
+    type: "Rally Point",
+    description: "",
+    distance: 475,
+    lastActive: "4 minutes ago",
+    icon: "bullhorn",
+    color: "green",
+  },
+  {
+    type: "Police Barricade",
+    description: "",
+    distance: 960,
+    lastActive: "2 minutes ago",
+    icon: "shield",
+    color: "blue",
+  },
+  {
+    type: "Police Arrests",
+    description: "",
+    distance: 1235,
+    lastActive: "11 minutes ago",
+    icon: "shield",
+    color: "blue",
+  },
+  {
+    type: "Looting",
+    description: "",
+    distance: 1405,
+    lastActive: "7 minutes ago",
+    icon: "fire",
+    color: "orange",
+  },
+];
+
+const EventList = ({ events }) => {
+  const listItems = useMemo(
+    () =>
+      sampleEvents.map((event, index) => (
+        <List.Item key={index}>
+          <List.Content floated="right">{event.lastActive}</List.Content>
+          <List.Content floated="left" verticalAlign="middle">
+            <Icon size="large" color={event.color} name={event.icon} />
+          </List.Content>
+          <List.Content>
+            <List.Header>{event.type}</List.Header>
+            <List.Description>
+              <b>{event.distance}ft</b>
+            </List.Description>
+          </List.Content>
+        </List.Item>
+      )),
+    [events]
+  );
+
+  return (
     <List inverted relaxed divided selection verticalAlign="middle">
-      <List.Item>
-        <List.Content floated="right">Just now</List.Content>
-        <Image as={Icon} size="large" color="red" name="medkit" />
-        <List.Content>
-          <List.Header>Water Distribution</List.Header>
-          <List.Description>
-            <b>250ft</b>
-          </List.Description>
-        </List.Content>
-      </List.Item>
-      <List.Item>
-        <List.Content floated="right">4 minutes ago</List.Content>
-        <Image as={Icon} size="large" color="green" name="bullhorn" />
-        <List.Content>
-          <List.Header>Rally Point</List.Header>
-          <List.Description>
-            <b>475ft</b>
-          </List.Description>
-        </List.Content>
-      </List.Item>
-      <List.Item>
-        <List.Content floated="right">2 minutes ago</List.Content>
-        <Image as={Icon} size="large" color="blue" name="shield" />
-        <List.Content>
-          <List.Header>Police Barricade</List.Header>
-          <List.Description>
-            <b>960ft</b>
-          </List.Description>
-        </List.Content>
-      </List.Item>
-      <List.Item>
-        <List.Content floated="right">11 minutes ago</List.Content>
-        <Image as={Icon} size="large" color="blue" name="shield" />
-        <List.Content>
-          <List.Header>Police Arrests</List.Header>
-          <List.Description>
-            <b>1235ft</b>
-          </List.Description>
-        </List.Content>
-      </List.Item>
-      <List.Item>
-        <List.Content floated="right">7 minutes ago</List.Content>
-        <Image as={Icon} size="large" color="orange" name="fire" />
-        <List.Content>
-          <List.Header>Looting</List.Header>
-          <List.Description>
-            <b>1405ft</b>
-          </List.Description>
-        </List.Content>
-      </List.Item>
+      {listItems}
     </List>
-    {/* </Segment> */}
-  </div>
-);
+  );
+};
 
 export default EventList;
